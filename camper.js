132d2a452d3a9c30b4bf6579b92d2c4ab6d1f016
Zi Wei Zhang <ziweidream@gmail.com>
2017-12-05 08:22:16 -0500

    Add files via upload

---
+++ camper.js
@@ -0,0 +1,90 @@
+const urlRecent = "https://fcctop100.herokuapp.com/api/fccusers/top/recent";
+const urlAllTime = "https://fcctop100.herokuapp.com/api/fccusers/top/alltime";
+const leadersRecent = getLeader(urlRecent);
+const leadersAllTime = getLeader(urlAllTime);
+
+function getLeader(url) {
+  let result = "";
+  $.ajax({
+    url: url,
+    async: false,
+    success: function(data) {
+      result = data;
+    }
+  });
+  const campers = JSON.parse(JSON.stringify(result));
+  const leaders = [];
+  for (let i = 0; i < 100; i++) {
+    leaders.push({
+      id: i + 1,
+      image: <img src={campers[i].img} width="25" height="25"/>,
+      name: campers[i].username,
+      pointsRecent: campers[i].recent,
+      pointsAll: campers[i].alltime
+    });
+  }
+  return leaders;
+}
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: leadersRecent
+    }
+  }
+
+  handleChange() {
+    this.setState({data: leadersAllTime})
+    let html = $('<span id="symbol">&starf;</span>');
+    $("#all>span").empty();
+    $("#all").append(html);
+    $("#past30>span").empty();
+  }
+
+  setDefault() {
+    this.setState({data: leadersRecent});
+    let html = $('<span id="symbol">&starf;</span>');
+    $("#past30>span").empty();
+    $("#past30").append(html);
+    $("#all>span").empty();
+  }
+
+  render() {
+    let rows = this.state.data.map(leader => {
+      return <LeaderRow key={leader.id
+} data={leader
+}/>
+    });
+    return (<table>
+      <caption>Leaderboard</caption>
+      <thead id="header">
+        <th>#</th>
+        <th id="name">Camper Name</th>
+        <th id="past30" onClick={() => this.setDefault()}>Points in past 30 days</th>
+        <th id="all" onClick={() => this.handleChange()}>All time points</th>
+      </thead>
+      < tbody >
+        {rows}
+        < /tbody>
+          < /table>)
+  }
+}
+const LeaderRow = (props) => {
+  return (<tr>
+      <td>
+        {props.data.id}
+      </td>
+      <td>
+        {props.data.image}{props.data.name}
+      </td>
+      <td>
+        {props.data.pointsRecent}
+      </td>
+      <td>
+        {props.data.pointsAll}
+      </td>
+    </tr>);
+    }
+
+ReactDOM.render( < App / > , document.getElementById('container'));
